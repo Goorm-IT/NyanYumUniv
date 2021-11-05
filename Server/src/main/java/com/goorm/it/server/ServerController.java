018ec8d3827da015ccb8fa66e8f27a87277f7c3c
taeminHan <taemin9705@gmail.com>
2021-11-05 20:14:14 +0900

    기타 파일 업로드

--- Server/src/main/java/com/goorm/it/server/ServerController.java
+++ Server/src/main/java/com/goorm/it/server/ServerController.java
@@ -1,18 +1,19 @@
 package com.goorm.it.server;
 
-
-
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import com.goorm.it.server.UserInfo.UserInfoDao;
+import com.goorm.it.server.RegisterUser.RegisterUserDao;
 
 
-//TODO: Main Controller, DAO, DTO(=VO) 공부하기
+//TODO: Main Controller, DAO, DTO(=VO) 공부하기, Session 유지 추가
 
 @RestController
 @SpringBootApplication
@@ -25,9 +26,14 @@ public class ServerController {
     @Autowired UserInfoDao infoDao;
 
     @RequestMapping("/select")
-
     public List<Map<String, ?>> getMessage(){
         return infoDao.selectAll();
     }
+
+    @RequestMapping("/register")
+    public List<Map<String, ?>> getRegister(){
+        return RegisterUserDao.add();
+    }
+
 
 }
