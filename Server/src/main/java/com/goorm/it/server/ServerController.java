5c8b89a13bd93320a4f4e9795a3394cbd1d242ea
taeminHan <taemin9705@gmail.com>
2021-12-09 18:48:56 +0900

    MainController 분리 및 간결화 작업

--- Server/src/main/java/com/goorm/it/server/ServerController.java
+++ Server/src/main/java/com/goorm/it/server/ServerController.java
@@ -1,46 +1,23 @@
 package com.goorm.it.server;
 
-
-
 import java.util.List;
 import java.util.Map;
 
-import com.goorm.it.server.RegisterUser.RegisterUserDao;
-import com.goorm.it.server.RegisterUser.RegisterUserDto;
-import com.goorm.it.server.UserInfo.UserInfoDto;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.goorm.it.server.UserInfo.UserInfoDao;
 
 
 //TODO: Main Controller, DAO, DTO(=VO) 공부하기
 
-@RestController
+@EnableAsync
 @SpringBootApplication
 public class ServerController {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerController.class, args);
     }
-
-    @Autowired UserInfoDao UIFD;
-
-    @RequestMapping("/select")
-    public List<Map<String, ?>> getMessage(){
-        return UIFD.selectAll();
-    }
-    @PostMapping("/selectUid")
-    public List<Map<String, Object>> checkUser(@RequestBody UserInfoDto userInfoDto){
-        return UIFD.checkUser(userInfoDto.getUid());
-    }
-
-    @PostMapping("/register")
-    public List<Map<String,?>> registerUser(@RequestBody RegisterUserDto registerUserDto){
-        return RegisterUserDao.add(registerUserDto.getUid(),registerUserDto.getNickName());
-    }
-
 
 
 }
