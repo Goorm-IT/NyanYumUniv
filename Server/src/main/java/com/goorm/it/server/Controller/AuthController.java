dfd2d7a55ac2e78b1d5125c7cf15b4f137e9208b
taeminHan <taemin9705@gmail.com>
2021-12-09 18:47:36 +0900

    Controller 분리 및 SpringSecurity Session 작업

---
+++ Server/src/main/java/com/goorm/it/server/Controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.goorm.it.server.Controller;
+
+
+import com.goorm.it.server.Login.LoginDto;
+import com.goorm.it.server.RegisterUser.RegisterUserDto;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping(value = "login", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String login(@RequestBody LoginDto loginDto){
+        return "login";
+    }
+
+    @PostMapping(value = "signup", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String signup(@RequestBody RegisterUserDto registerUserDto){
+        return "signup";
+    }
+}
