c8ee8cb9f397ca99b18132ef85f721bf24791c3f
kimseokho582 <gjrehf@naver.com>
2022-11-30 21:05:04 +0900

    ios naver 로그인 수정

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -4,11 +4,18 @@
 
 @implementation AppDelegate
 
+
+
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+// Implemented when iOS 9.0 Less
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [[NaverThirdPartyLoginConnection getSharedInstance] application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
 }
 
 // Implemented when iOS 9.0 higher
